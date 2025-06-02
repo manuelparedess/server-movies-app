345625895940747862b24e3983c29b54dc365838
Manuel <luisparedescuenca@gmail.com>
2025-06-02 18:02:55 -0300

    More movies per page when using getMovies

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -28,7 +28,7 @@ const getMovies = async (req, res) => {
 
     //Obtenemos los parametros
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 5;
+    const limit = parseInt(req.query.limit) || 6;
 
     //Las peliculas de cada pagina
     const skip = (page - 1) * 5;
